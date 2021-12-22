d7fde08ea4afaec7438affd6dda9ecc3d2644381
SneakyJ <k.nikolaienko@gmail.com>
2021-12-22 20:55:40 +0100

    Added components: header, menu, navigation, progress-bar, task-stats, footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { FooterComponent } from './footer/footer.component';
+import { MenuComponent } from './menu/menu.component';
+import { ProgressBarComponent } from './progress-bar/progress-bar.component';
+import { TaskStatsComponent } from './task-stats/task-stats.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavigationComponent,
+    FooterComponent,
+    MenuComponent,
+    ProgressBarComponent,
+    TaskStatsComponent
   ],
   imports: [
     BrowserModule,
