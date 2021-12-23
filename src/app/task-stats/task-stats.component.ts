8b6cd5c51d9f9456fcc3b6c221f1edae35e7f0ef
SneakyJ <k.nikolaienko@gmail.com>
2021-12-23 11:55:30 +0100

    Fixes related with changing css to  SCSS

--- src/app/task-stats/task-stats.component.ts
+++ src/app/task-stats/task-stats.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-task-stats',
   templateUrl: './task-stats.component.html',
-  styleUrls: ['./task-stats.component.css']
+  styleUrls: ['./task-stats.component.scss']
 })
 export class TaskStatsComponent implements OnInit {
 
