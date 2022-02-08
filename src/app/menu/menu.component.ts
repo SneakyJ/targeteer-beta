680205926cc4bf7c45fe767cd06d7c0ed931850f
SneakyJ <k.nikolaienko@gmail.com>
2022-02-08 07:08:35 +0100

    Menu

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -9,6 +9,15 @@ export class MenuComponent implements OnInit {
 
   constructor() { }
 
+  // menuItems: any[] = [
+  //   {"name":'Menu item 1'}, {"name":'Menu item 2'}, {"name":'Menu item 3'}];
+
+  menuItems: string[] = [
+    'item0', 
+    'item1', 
+    'item2', 
+    'item3'];
+
   ngOnInit(): void {
   }
 
